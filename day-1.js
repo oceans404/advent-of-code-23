1a11446e8be8cc93fccb0c9109ed0086210cca96
oceans404 <steph.orpilla@nillion.com>
2023-12-01 11:55:02 -0800

    solved day 1

--- day-1.js
+++ day-1.js
@@ -1,8 +1,5 @@
 const readInputToArray = require('./inputHelper');
 
-const arr = readInputToArray('day-1');
-console.log(arr);
-
 const exampleInput = [
     '1abc2', //12
     'pqr3stu8vwx', // 38
@@ -10,9 +7,52 @@ const exampleInput = [
     'treb7uchet' // 77
 ]
 
-// trebuchet(exampleInput) = 142
+const isNum = (char) => !isNaN(parseInt(char));
 
-const trebuchet = () => {
+const trebuString = (str) => {
+    let firstNum, lastNum;
+    let foundIndex = 0;
 
+    // counting up
+    // from 0 index, get the first number
+    for (let i=0; i<str.length; i++) {
+        const char = str[i];
+        if (isNum(char)) {
+            firstNum = char;
+            lastNum = char; // set lastNum in case one isn't found
+            foundIndex = i;
+            break; // Breaks the loop if a number is found
+        }
+        
+    }
+
+    // counting down but stop once you get to the point the count up got to
+    // from n-1 index, get the first number counting down to foundIndex
+    for (let i = str.length - 1; i >= foundIndex; i--) {
+        const char = str[i]
+        if (isNum(char)) {
+            lastNum = char;
+            if (!firstNum) {
+                firstNum = char; // set firstNum if one wasn't found
+            }
+            break; // Breaks the loop if a number is found
+        }
+    }
+    
+    return parseInt(firstNum + lastNum)
 }
 
+console.log(trebuString(exampleInput[0])) // 12
+console.log(trebuString(exampleInput[1])) // 38
+console.log(trebuString(exampleInput[2])) // 15
+console.log(trebuString(exampleInput[3])) // 77
+
+const trebuchet = (arr) => {
+    const total = arr.map(trebuString).reduce((acc, curr) => acc + curr, 0);
+    console.log(total);
+}
+
+trebuchet(exampleInput) // 142
+const inputArr = readInputToArray('day-1');
+console.log(inputArr)
+trebuchet(inputArr) // 55971
